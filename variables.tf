b699010ab2299ad609376883de80926d4b6b7ccc
MalarvizhiK <k.malarvizhi@in.ibm.com>
2020-12-17 19:53:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,68 +18,12 @@ variable "resource_group" {
   description = "The resource group to use. If unspecified, the account's default resource group is used."
 }
 
-variable "vpc_id" {
-  default     = ""
-  description = "The vpc id to use." 
-}
-
 variable "vpc_url" {
   default     = "https://us-south.iaas.cloud.ibm.com"
   description = "The public endpoint url of VPC."
 }
 
-variable "apikey" {
+variable "private_ssh_key" {
   default     = ""
-  description = "The apikey of IBM Cloud account."
+  description = "The private ssh key string to use."
 }
-
-variable "ssh_key" {
-  default     = "my-ssh-key"
-  description = "The ssh key to use. If unspecified, 'my-ssh-key' is used."
-}
-
-variable "private_ssh_key_file" {
-  default     = "~/.ssh/id_rsa"
-  description = "The private ssh key file to use. If unspecified, '~/.ssh/id_rsa' is used."
-}
-
-variable "ubuntu_subnet_id" {
-  default     = ""
-  description = "subnet1 ipv4 cidr block."
-}
-
-variable "mgmt_ip1" {
-  default     = ""
-  description = "The management IP 1 of VNF."
-}
-
-variable "ext_ip1" {
-  default     = ""
-  description = "The external IP 1 of VNF."
-}
-
-variable "mgmt_ip2" {
-  default     = ""
-  description = "The management IP 2 of VNF."
-}
-
-variable "ext_ip2" {
-  default     = ""
-  description = "The external IP 2 of VNF."
-}
-
-variable "ha_subnet_ipv4_cidr_block" {
-  default     = ""
-  description = "HA subnet ipv4 cidr block."
-}
-
-variable "ha_password1" {
-  default     = ""
-  description = "HA instance1 Password."
-}
-
-variable "ha_password2" {
-  default     = ""
-  description = "HA instance2 Password."
-}
-
